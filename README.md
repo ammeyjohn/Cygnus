8634349654df0e08c5c4e579f75648a451de4e39
Patrick <ammeyjohn@gmail.com>
2017-10-19 22:59:24 +0800

    Construct the basic project structure.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Cygnus
 A product release manage system.
+
+## Reference
+
++ 从零搭建vue2+vue-router2+webpack3工程
+http://www.qinshenxue.com/article/20161118151423.html
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'build.js'
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            use: ["vue-loader"]
+        }, {
+            test: /\.css$/,
+            use: ["vue-style-loader", "css-loader"]
+        }]
+    },
+    plugins: []
+}
