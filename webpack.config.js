b3a79766eef10048e9381c34f825c0ac6b6de2e8
yuanjie <yuanjie@shanghai3h.com>
2017-10-20 17:49:43 +0800

    Construct the basic project structure. Adjust webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@
 var path = require('path');
 var webpack = require('webpack');
 
+// Plugins
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/main.js',
     output: {
@@ -13,11 +17,43 @@ module.exports = {
     module: {
         rules: [{
             test: /\.vue$/,
-            use: ["vue-loader"]
+            use: "vue-loader"
+        }, {
+            test: /\.js$/,
+            use: "babel-loader",
+            include: [path.resolve(__dirname, 'src')]
         }, {
             test: /\.css$/,
-            use: ["vue-style-loader", "css-loader"]
+            use: ExtractTextPlugin.extract({
+                use: ["vue-style-loader", "css-loader"]
+            })
+        }, {
+            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+            use: [{
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    name: 'images/[name].[hash:7].[ext]'
+                }
+            }]
+        }, {
+            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+            use: [{
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    name: 'fonts/[name].[hash:7].[ext]'
+                }
+            }]
         }]
     },
-    plugins: []
+    plugins: [
+        new ExtractTextPlugin({
+            filename: "css/style.css"
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.tpl.html'
+        })
+    ]
 }
