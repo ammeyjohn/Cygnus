d1af64230806a7cd1296e3f3798e59149b3bbffa
Patrick <ammeyjohn@gmail.com>
2017-10-25 22:05:35 +0800

    [Temp] Build header.

--- src/index/index.vue
+++ src/index/index.vue
@@ -1,5 +1,10 @@
 <template>
-    <sidebar-menu></sidebar-menu>
+    <div>
+        <sidebar-menu></sidebar-menu>
+        <div class="main-header-con">
+            <div class="main-header">asdfasdfasd</div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -13,5 +18,22 @@
 </script>
 
 <style>
+    .main-header-con {
+        box-sizing: border-box;
+        position: fixed;
+        display: block;
+        padding-left: 200px;
+        width: 100%;
+        height: 100px;
+        z-index: 1000;
+        box-shadow: 0 2px 1px 1px rgba(100,100,100,.1);
+    }
 
+    .main-header {
+        height: 60px;
+        background: #fff;
+        box-shadow: 0 2px 1px 1px rgba(100,100,100,.1);
+        position: relative;
+        z-index: 10;
+    }
 </style>
