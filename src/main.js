074ad172ba65f8e54cbcf9ab482a37afaa97a444
yuanjie <yuanjie@shanghai3h.com>
2017-10-25 20:47:32 +0800

    [Temp] Moves menu template to single file.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue';
-// import iView from 'iview';
-// import 'iview/dist/styles/iview.css';
-
+import VueRouter from 'vue-router';
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 
 import App from './app.vue';
 
-// Vue.use(iView);
+Vue.use(VueRouter);
 Vue.use(ElementUI);
 
 new Vue({
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
         main: './src/main.js',
         vendor: [
             'vue',
+            'VueRouter',
             'iview'
         ]
     },
