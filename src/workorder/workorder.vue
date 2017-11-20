d3cf42077005dd102f3e7c0838e40e4934e8506b
yuanjie <yuanjie@shanghai3h.com>
2017-11-20 14:05:16 +0800

    Restruct the layout by iview for workorder page.

--- src/workorder/workorder.vue
+++ src/workorder/workorder.vue
@@ -1,156 +1,102 @@
-<style>
-/* .container-fluid .control-label {
-    text-align: left;
-} */
-</style>
-
-
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-8">
-                <Card>
-                    <p slot="title">工单内容</p>
-                    <form class="form-horizontal">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="project" class="col-md-3 control-label">项目名称</label>
-                                    <div class="col-md-9">
-                                        <Select id="project" size="large" v-model="order.projectId" filterable @on-change="onProjectSelectChange">
-                                            <Option v-for="item in projects" :value="item.id" :key="item.code">{{ item.code + ' ' + item.name }}</Option>
+    <Row :gutter="10">
+        <Col :xs="24" :sm="24" :md="18">
+            <Card>
+                <p slot="title">工单内容</p>
+                <Form :label-width="100" :rules="ruleValidate">
+                    <Row>
+                        <Col span="12">                      
+                            <FormItem label="项目名称" prop="project">
+                                <Select id="project" v-model="order.projectId" filterable @on-change="onProjectSelectChange">
+                                    <Option v-for="item in projects" :value="item.id" :key="item.code">{{ item.code + ' ' + item.name }}</Option>
+                                </Select>
+                            </FormItem>
+                        </Col>
+                        <Col span="12">
+                            <FormItem label="产品名称" prop="product">
+                                <Select id="product" v-model="order.productId" placeholder="请选择项目对应的产品" filterable>
+                                    <Option v-for="item in products" :value="item.id" :key="item.code">{{ item.code + ' ' + item.name }}</Option>
+                                </Select>                                
+                            </FormItem>                        
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span="12">
+                            <FormItem label="工单类型" prop="orderType">
+                                <Select id="orderType" v-model="order.type" placeholder="请选择工单类型">
+                                    <Option v-for="item in orderTypes" :value="item.id" :key="item.code">{{ item.code + ' ' + item.name }}</Option>
+                                </Select>
+                            </FormItem>
+                        </Col>
+                        <Col span="12">
+                            <FormItem label="事件日期" prop="eventDate">
+                                <DatePicker type="date" placeholder="请选择事件发生日期" style="display: block"></DatePicker>
+                            </FormItem>                        
+                        </Col>
+                    </Row>
+                    <FormItem label="工单标题" prop="title">
+                        <Row>
+                            <Col span="18">
+                                <FormItem prop="title">
+                                    <Input id="title" v-model="order.title" placeholder="请对问题进行简短的描述"></Input>
+                                </FormItem>
+                            </Col>
+                            <Col span="6">
+                                <FormItem prop="duration">
+                                    <Input>
+                                        <span slot="prepend">
+                                            <Icon type="clock" style="font-size: 16px;"></Icon>
+                                        </span>
+                                        <Select slot="append" style="width: 70px">
+                                            <Option value="minute">分钟</Option>
+                                            <Option value="hour">小时</Option>
+                                            <Option value="day">天</Option>
                                         </Select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="product" class="col-md-3 control-label">产品名称</label>
-                                    <div class="col-md-9">
-                                        <Select id="product" v-model="order.productId" placeholder="请选择项目对应的产品" filterable>
-                                            <Option v-for="item in products" :value="item.id" :key="item.code">{{ item.code + ' ' + item.name }}</Option>
-                                        </Select>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="type" class="col-md-3 control-label">工单类型</label>
-                                    <div class="col-md-9">
-                                        <Select id="type" size="large" v-model="order.type" placeholder="请选择工单类型">
-                                            <Option v-for="item in orderTypes" :value="item.id" :key="item.code">{{ item.code + ' ' + item.name }}</Option>
-                                        </Select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="date" class="col-md-3 control-label">事件日期</label>
-                                    <div class="col-md-9">
-                                        <DatePicker type="date" size="large" placeholder="请选择事件发生日期" style="display: block"></DatePicker>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label for="title" class="col-md-2 control-label">工单标题</label>
-                                    <div class="col-md-7">
-                                        <input id="title" class="form-control" v-model="order.title" placeholder="请对问题进行简短的描述"></input>
-                                    </div>
-                                    <div class="col-md-3">
-                                        <div class="input-group">
-                                            <div class="input-group-addon">
-                                                <i class="glyphicon glyphicon-time"></i>
-                                            </div>
-                                            <input id="title" class="form-control" v-model="order.title" placeholder="请输入工时"></input>
-                                            <div class="input-group-btn">
-                                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">分钟 <span class="caret"></span></button>
-                                                <ul class="dropdown-menu dropdown-menu-right">
-                                                    <li><a href="#">分钟</a></li>
-                                                    <li><a href="#">小时</a></li>
-                                                    <li><a href="#">天</a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label for="content" class="col-lg-2 control-label">工单内容</label>
-                                    <div class="col-lg-10">
-                                        <textarea id="content" class="form-control" v-model="order.content" rows="20" placeholder="请详细描述问题的表现、重现步骤和现场环境等"></textarea>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label for="tags" class="col-md-2 control-label">工单标签</label>
-                                    <div class="col-md-10">
-                                        <Input id="tags" type="text" placeholder="请添加工单标签"></Input>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row" style="margin-top: 10px">
-                            <div class="col-md-12">
-                                <button type="button" class="btn btn-primary" style="float:right;" v-on:click="save">保存</button>
-                            </div>
-                        </div>
-                    </form>
-                </Card>
-            </div>
-            <div class="col-md-4">
-                <div class="row">
-                    <div class="col-md-12">
-                        <Card>
-                            <p slot="title">
-                                <Icon type="document-text"></Icon>
-                                工单信息
-                            </p>
-                            <div class="row">
-                                <div class="col-md-12 form-group">
-                                    <label for="state" class="col-sm-3 control-label" style="margin-left: -15px;">工单状态</label>
-                                    <div class="col-sm-9">
-                                        <i-switch id="state" size="large" :value="order.state">
-                                            <span slot="open">开启</span>
-                                            <span slot="close">关闭</span>
-                                        </i-switch>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12 form-group">
-                                    <label for="promoter">发起人</label>
-                                    <Input id="promoter" type="text" placeholder="请输入工单发起人" :value="order.promoter"></Input>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12 form-group">
-                                    <label for="handler">处理人</label>
-                                    <Input id="handler" type="text" placeholder="请输入工单处理人" :value="order.handler"></Input>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12 form-group">
-                                    <label for="notifiers">通知列表</label>
-                                    <Input id="notifiers" type="text" placeholder="请输入需要接收通知的人员"></Input>
-                                </div>
-                            </div>
-                        </Card>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                                    </Input>
+                                </FormItem>
+                            </Col>
+                        </Row>
+                    </FormItem>
+                    <FormItem label="工单内容" prop="content">
+                        <Input type="textarea" :rows="10"></Input>
+                    </FormItem>
+                    <FormItem label="工单标签" prop="tags">
+                        <Tag v-for="item in 5" :key="item" :name="item" closable>标签{{ item + 1 }}</Tag>
+                        <Button icon="ios-plus-empty" type="dashed" size="small">添加标签</Button>                        
+                    </FormItem>
+                    <FormItem style="text-align:right;">
+                        <Button type="primary">提交</Button>
+                        <Button type="ghost" style="margin-left: 8px">取消</Button>
+                    </FormItem>                    
+                </Form>
+            </Card>
+        </Col>
+        <Col :xs="24" :sm="24" :md="6">
+            <Card>                
+                <p slot="title">
+                    <Icon type="document-text"></Icon>
+                    工单信息
+                </p>
+                <Form>
+                    <FormItem label="工单状态">
+                        <i-switch id="state" size="large" :value="order.state">
+                            <span slot="open">开启</span>
+                            <span slot="close">关闭</span>
+                        </i-switch>
+                    </FormItem>
+                    <FormItem label="发起人">
+                        <Input id="promoter" type="text" placeholder="请输入工单发起人" :value="order.promoter"></Input>
+                    </FormItem>
+                    <FormItem label="处理人">
+                        <Input id="handler" type="text" placeholder="请输入工单处理人" :value="order.handler"></Input>
+                    </FormItem>  
+                    <FormItem label="通知列表">
+                        <Input id="notifiers" type="text" placeholder="请输入需要接收通知的人员"></Input>
+                    </FormItem>
+                </Form>
+            </Card>        
+        </Col>
+    </Row>
 </template>
 
 <script>
@@ -162,6 +108,14 @@
     export default {
         data() {
             return {
+                ruleValidate: {
+                    project: [ { required: true, message: '请选择项目', trigger: 'change' } ],
+                    product: [ { required: true, message: '请选择项目对应的产品', trigger: 'change' } ],
+                    orderType: [ { required: true, message: '请选择工单类型', trigger: 'change' } ],
+                    eventDate: [ { required: true, message: '请填写事件发生日期', trigger: 'change' } ],
+                    title: [ { required: true, message: '请填写工单标题', trigger: 'blur' } ],
+                    content: [ { required: true, message: '请填写工单内容', trigger: 'blur' } ]
+                },                
                 projects: [],
                 products: [],
                 orderTypes: [],
@@ -174,7 +128,7 @@
                     date: '',
                     promoter: 1,
                     handler: 1,
-                    state: 1,
+                    state: '开启',
                     notifiers: [],
                     tags: [],
                     createUser: 1,
